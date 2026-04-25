d5bf6e984874580ba21d2436ad73f9380a72a384
romarioproxxima <romario.proxxima@gmail.com>
2026-04-25 12:55:00 -0300

    Bump SW v2.3.1

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache do app shell pra funcionar offline.
 // Estratégia: stale-while-revalidate pros assets estáticos, network-first pra análise de IA.
 
-const VERSION = "v2.3.0";
+const VERSION = "v2.3.1";
 const CACHE = "diariofit-" + VERSION;
 
 const SHELL = [
